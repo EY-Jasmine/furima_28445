64a7567339d314ab6c5c51369a79cfa33d06d6dd
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-08-14 14:51:03 +0900

    READMEの訂正(emailを一意性に)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< Updated upstream
-ActiveRecord::Schema.define(version: 2020_08_13_103455) do
-=======
 ActiveRecord::Schema.define(version: 2020_08_13_112440) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -29,7 +26,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_112440) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
->>>>>>> Stashed changes
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,8 +39,5 @@ ActiveRecord::Schema.define(version: 2020_08_13_112440) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< Updated upstream
-=======
   add_foreign_key "items", "users"
->>>>>>> Stashed changes
 end

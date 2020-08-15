baa8569ced179d53636ba4c67644c31dd474b724
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-08-15 17:38:29 +0900

    ストロングパラメータの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:myoji_kana])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:myoji_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:namae_kana])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:namae_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:birthday])
+  end
+
 end

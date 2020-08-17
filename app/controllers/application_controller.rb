7cc616008a63a96e1ca3245723920a347e7223bd
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-08-17 16:03:32 +0900

    パスワード文字種制限の訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # パスワードに英字と数字の両方を含める
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
-  # 名前（姓名）に全角文字以外を使えないようにする
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :family_name
-    validates :first_name
-  end
-  # 名前（ふりがな）に全角カタカナ以外を使えないようにする
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
-    validates :myoji_kana
-    validates :namae_kana
-  end 
-
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # パスワードに英字と数字の両方を含める
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  # 名前（姓名）に全角文字以外を使えないようにする
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  # 名前（ふりがな）に全角カタカナ以外を使えないようにする
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+    validates :myoji_kana
+    validates :namae_kana
+  end 
+  
 end

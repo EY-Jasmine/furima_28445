6f0c542afde7f92725f6d85bc07e97f38ee5c7e8
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-08-20 14:19:08 +0900

    バリデーション、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  # 全カラム空欄を禁止
+  with_options presence: true do  
+    validates :name
+    validates :detail
+    validates :category
+    validates :condition
+    validates :delivery_fee
+    validates :prefecture
+    validates :day
+    validates :price
+    validates :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :items
 
   # 全カラム空欄を禁止
   # ニックネームとメールアドレスを一意性に

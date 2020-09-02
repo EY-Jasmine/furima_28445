5d794f963c2bd02c2e6cc1dd44178ef799b8fa32
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-09-02 13:30:40 +0900

    削除に失敗した時の処理を指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,13 @@ class ItemsController < ApplicationController
   
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.user_id == current_user.id
+      if item.destroy
+       redirect_to root_path
+      else
+       redirect_to item_path(@item.id)
+      end
+    end
   end
 
   private

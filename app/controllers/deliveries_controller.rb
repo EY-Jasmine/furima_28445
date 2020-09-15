677c1b4b27c3d699b36e462abe62bcbb49945cd5
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-09-15 18:55:46 +0900

    購入ボタンからdelivery_indexの表示

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,11 +1,25 @@
 class DeliveriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :delivery_pickup, only: [:show, :edit, :update]
+
+  def index
+    @deliveries = Delivery.all
+  end
+  
+  def new
+    @delivery = Delivery.new
+  end
+
   def create
     Delivery.create(delivery_params)
-    redirect_to root_path
   end
 
   private
   def delivery_params
-    params.require(:delivery).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:delivery).merge(user_id: current_user.id, delivery_id: params[:delivery_id])
+  end
+
+  def item_pickup
+    @delivery = Delivery.find(params[:id])
   end
 end

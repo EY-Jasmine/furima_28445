05db3227e404de4b752a2aaa6b82e8f574f3081d
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-09-17 12:25:32 +0900

    バリデーション空欄NG

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -2,4 +2,20 @@ class Delivery < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to :item
+
+  # 空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :zip_code
+    validates :prefecture
+    validates :city
+    validates :address
+    validates :tel
+    validates :item
+  end
+
+  # ジャンルの選択が「--」の時は保存できないようにする
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+  end
+
 end

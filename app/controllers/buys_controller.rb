1fc4fbf196819ebfa90bff0fdaaececfb2c51768
EY-Jasmine <s81.yoshida.emi@gmail.com>
2020-09-25 16:25:55 +0900

    購入画面で商品情報の表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,11 +1,23 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!
+  before_action :item_pickup, only: [:index, :create]
+
+# itemテーブルからdeliveryへデータを持ってくる
+# item_id毎に個別のデータを持ってこれるようにする
+  def index
+  end
+
   def create
-    Buy.create(buy_params)
-    redirect_to root_path
+    Delivery.create(delivery_params)
   end
 
   private
-  def buy_params
-    params.require(:buy).merge(user_id: current_user.id, item_id: params[:item_id])
+  def delivery_params
+    params.require(:delivery).permit(:zip_code, :prefecture_id, :city, :address, :building, :tel)
+    .merge(user_id: current_user.id, delivery_id: params[:delivery_id])
+  end
+
+  def item_pickup
+    @item = Item.find(params[:item_id])
   end
 end
